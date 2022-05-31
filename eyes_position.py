bc9b75413e43809dcb1851327f0435e067ba2ca8
felipeadsm <97059009+felipeadsm@users.noreply.github.com>
2022-05-31 11:53:28 -0300

    update: Comentários no código

--- eyes_position.py
+++ eyes_position.py
@@ -5,16 +5,17 @@ import math
 
 mp_face_mesh = mp.solutions.face_mesh
 
-LEFT_EYE = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
-RIGHT_IRIS = [474, 475, 476, 477]
-
+# Right Eyes Points
 RIGHT_EYE = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
-LEFT_IRIS = [469, 470, 471, 472]
-
-L_H_LEFT = [33]
-L_H_RIGHT = [133]
+RIGHT_IRIS = [474, 475, 476, 477]
 R_H_LEFT = [362]
 R_H_RIGHT = [263]
+
+# Left Eyes Points
+LEFT_EYE = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
+LEFT_IRIS = [469, 470, 471, 472]
+L_H_LEFT = [33]
+L_H_RIGHT = [133]
 
 
 def euclidian_distance(point_one, point_two):
@@ -24,17 +25,24 @@ def euclidian_distance(point_one, point_two):
     return distance
 
 
-def iris_position(iris_center, right_point, left_point):
+def iris_position(iris_center, right_point, left_point, eye):
+    position_iris = ''
+    ratio = None
+
     center_to_right_distance = euclidian_distance(iris_center, right_point)
     center_to_left_distance = euclidian_distance(iris_center, left_point)
     total_distance = euclidian_distance(right_point, left_point)
-    ratio = center_to_right_distance / total_distance
-    position_iris = ''
+
+    if eye == 'left':
+        ratio = center_to_left_distance / total_distance
+    elif eye == 'right':
+        ratio = center_to_right_distance / total_distance
+
     if ratio <= 0.42:
         position_iris = 'right'
     elif 0.42 < ratio <= 0.57:
         position_iris = 'center'
-    else:
+    elif ratio > 0.57:
         position_iris = 'left'
 
     return position_iris, ratio
@@ -66,27 +74,33 @@ with mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection
             # cv2.polylines(frame, [mesh_points[LEFT_IRIS]], True, (255, 0, 255), 1, cv2.LINE_AA)
             # cv2.polylines(frame, [mesh_points[RIGHT_IRIS]], True, (255, 0, 255), 1, cv2.LINE_AA)
 
-            # Iris Detection: Circle
+            # Iris Detection: Create Circle
             (l_cx, l_cy), l_radius = cv2.minEnclosingCircle(mesh_points[LEFT_IRIS])
             (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(mesh_points[RIGHT_IRIS])
             center_left = np.array([l_cx, l_cy], dtype=np.int32)
             center_right = np.array([r_cx, r_cy], dtype=np.int32)
+
+            # Iris Detection: Circle
             cv2.circle(frame, center_left, int(l_radius), (255, 0, 255), 1, cv2.LINE_AA)
             cv2.circle(frame, center_right, int(r_radius), (255, 0, 255), 1, cv2.LINE_AA)
 
+            # Iris Detection: Right Points
             cv2.circle(frame, mesh_points[R_H_RIGHT][0], 3, (255, 0, 255), -1, cv2.LINE_AA)
             cv2.circle(frame, mesh_points[R_H_LEFT][0], 3, (0, 255, 255), -1, cv2.LINE_AA)
 
+            # Iris Detection: Left Points
             cv2.circle(frame, mesh_points[L_H_RIGHT][0], 3, (0, 255, 255), -1, cv2.LINE_AA)
             cv2.circle(frame, mesh_points[L_H_LEFT][0], 3, (255, 0, 255), -1, cv2.LINE_AA)
 
-            right_iris_pos, ratio_r = iris_position(center_right, mesh_points[R_H_RIGHT], mesh_points[R_H_LEFT][0])
-            left_iris_pos, ratio_l = iris_position(center_left, mesh_points[L_H_LEFT], mesh_points[L_H_RIGHT][0])
+            # Iris Detection: Position
+            left_iris_pos, ratio_l = iris_position(center_left, mesh_points[L_H_LEFT],
+                                                   mesh_points[L_H_RIGHT][0], 'left')
+            right_iris_pos, ratio_r = iris_position(center_right, mesh_points[R_H_RIGHT],
+                                                    mesh_points[R_H_LEFT][0], 'right')
 
-            # print('Left Eye: ' + left_iris_pos + '|| Right Eye: ' + right_iris_pos)
-
+            # Iris Detection: Result With Detection
             cv2.putText(frame, 'Left Iris Position: {} | Right Iris Position {}'.format(left_iris_pos, right_iris_pos),
-                        (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+                        (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 1, cv2.LINE_AA)
 
         cv2.imshow('image', frame)
         key = cv2.waitKey(1)
